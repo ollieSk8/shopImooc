c06c122c4963ddb220e2e9e207bcf37df5f58203
ollieSk8 <412537738@qq.com>
2016-03-31 00:06:59 +0800

    查看管理员增加了分页功能

--- configs/configs.php
+++ configs/configs.php
@@ -2,6 +2,6 @@
 	define("DB_HOST","localhost");
 	define("DB_USER","root");
 	define("DB_PWD","root");
-	define("DB_NAME","shopimooc");
+	define("DB_NAME","shopImooc");
 	define("DB_CHARSET","utf8");
 ?>
--- lib/page.func.php
+++ lib/page.func.php
@@ -1,67 +1,56 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>分页</title>
-	<link rel="stylesheet" type="text/css" href="../admin/styles/bootstrap.min.css">
-</head>
-<body>
-	<?php 
-		require_once"../include.php";
-
-		$step=3;
-		$sql="select *from imooc_admin";
-		$totalPage=ceil(getResultNum($sql)/$step);
-		$url=$_SERVER["PHP_SELF"];
-		//如果没传页数则默认是第一页，如果传了查看是否非法，做非法超出处理，都没有按传进来的数字处理
-		if(@$_REQUEST["page"]){
-			if($_REQUEST["page"]<1){
-				$reqNum=1;
-			}elseif($_REQUEST["page"]>$totalPage){
-				$reqNum=$totalPage;
-			}else{
-				$reqNum=$_REQUEST["page"];
+<?php 
+		//@$url 当前文件路径
+		//@$reqNum 当前传进来的页数
+		//$totalPage 当前总页数
+		function renderPagination($table,$step=3){
+		    $sql="select *from {$table}";
+		    $totalPage=ceil(getResultNum($sql)/$step);
+		    //如果没传页数则默认是第一页，如果传了查看是否非法，做非法超出处理，都没有按传进来的数字处理
+		    if(@$_REQUEST["page"]){
+		      if($_REQUEST["page"]<1){
+		        $reqNum=1;
+		      }elseif($_REQUEST["page"]>$totalPage){
+		        $reqNum=$totalPage;
+		      }else{
+		        $reqNum=$_REQUEST["page"];
+		      }
+		      
+		    }else{
+		      $reqNum=1;
+		    }
+		    $page=($reqNum-1)*$step;
+		    $sql="select *from {$table} limit {$page},{$step}";
+		    $rows=fetch_all($sql);
+			$url=$_SERVER["PHP_SELF"];
+			$html="";
+			$html.= "<nav>";
+		    $html.=     "<ul class=\"pagination\">";
+		    if($reqNum==1){
+		    	$html.=  "<li class=\"disabled\"><a aria-label=\"Previous\"><span>&laquo;</span></a></li>";
+		    }else{
+		    	$html.=  "<li><a href=\"".$url."?page=".($reqNum-1)."\" aria-label=\"Previous\"><span>&laquo;</span></a></li>";
+		    }
+		    for($i=0;$i<$totalPage;$i++){
+		    	if($i+1==$reqNum){
+		    		$html.=  " <li class=\"active\"><span>".$reqNum."</span></li>";
+		    	}else{
+		    		$html.=  "<li><a href=\"".$url."?page=".($i+1)."\">".($i+1)."</a></li>";
+		    	}
+		    }
+		    if($reqNum==$totalPage){
+		    	$html.=  "<li class=\"disabled\"><a aria-label=\"Next\"><span>&raquo;</span></a></li>";
+		    }else{
+		    	$html.=  "<li><a href=\"".$url."?page=".($reqNum+1)."\" aria-label=\"Next\"><span>&raquo;</span></a></li>";
+		    }
+		    $html.=    "</ul>";
+		    $html.=  "</nav>";
+		    if($totalPage<=1){
+				$html="";
 			}
-			
-		}else{
-			$reqNum=1;
-		}
-		$page=($reqNum-1)*$step;
-		// 1 3  6
-		$sql="select *from imooc_admin limit {$page},{$step}";
-		$rows=fetch_all($sql);
-
-		foreach ($rows as $key => $value) {
-			foreach ($value as $k => $v) {
-				echo $k."--".$v."<br>";
-			}
-			echo "<hr>";
-		}
-
-
-		echo "<nav>";
-	    echo     "<ul class=\"pagination\">";
-	    if($reqNum==1){
-	    	echo "<li class=\"disabled\"><a aria-label=\"Previous\"><span>&laquo;</span></a></li>";
-	    }else{
-	    	echo "<li><a href=\"".$url."?page=".($reqNum-1)."\" aria-label=\"Previous\"><span>&laquo;</span></a></li>";
-	    }
-	    
-	          // <li class="disabled"><a href="http://www.baidu.com" aria-label="Previous"><span>&laquo;</span></a></li>
-	          // <li class="active"><span>1</span></li>
-	          // <li><a href="#">2</a></li>
-	          // <li><a href="#">3</a></li>
-	          // <li class="disabled"><a href="#" aria-label="Next"><span>&raquo;</span></a></li>
-	    if($reqNum==$totalPage){
-	    	echo "<li class=\"disabled\"><a aria-label=\"Next\"><span>&raquo;</span></a></li>";
-	    }else{
-	    	echo "<li><a href=\"".$url."?page=".($reqNum+1)."\" aria-label=\"Next\"><span>&raquo;</span></a></li>";
-	    }
-	    echo   "</ul>";
-	    echo "</nav>";
-
-	?>
-	
-</body>
-</html>
+		    return array(
+		    	"rows"=>$rows,
+		    	"pagination"=>$html
+		    );
+		}		
+?>
 
